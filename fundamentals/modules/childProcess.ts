51cf760b841305a9d6811a2571eda81033298285
carlosayol <Carlosaolar@gmail.com>
2022-10-25 19:06:36 -0500

    child processes

---
+++ fundamentals/modules/childProcess.ts
@@ -0,0 +1,10 @@
+import exec from "child_process"
+
+exec("node modules/errors.ts", (err, stdout, sterr) => {
+    if (err) {
+        console.log(err)
+        return false
+    }
+
+    console.log(stdout)
+})
