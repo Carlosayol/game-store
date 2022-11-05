1fd185137b228e9a9608a38e18cf80a630382415
carlosayol-slang <carlos.ayala@startupslang.com>
2022-11-04 19:09:56 -0500

    check how parameters work in endpoints

--- nest/game-store/src/app.controller.ts
+++ nest/game-store/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
@@ -15,9 +15,16 @@ export class AppController {
     return 'This is new'
   }
 
-  // the methods work with slash or without it when resolving routes
-  @Get('/a/')
-  hello(): string {
-    return 'with /a/'
+  @Get('products/:id')
+  getProduct(@Param('id') id: string): string {
+    return `product ${id}`
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(
+    @Param('id') id: string,
+    @Param('productId') productId: string,
+  ): string {
+    return `category ${id} and product ${productId}`
   }
 }
