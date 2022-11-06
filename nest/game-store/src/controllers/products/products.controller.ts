8b712a587e9deb5ea1c2e5004c0bdd3083d6d780
carlosayol-slang <carlos.ayala@startupslang.com>
2022-11-06 11:21:10 -0500

    complete product crud

--- nest/game-store/src/controllers/products/products.controller.ts
+++ nest/game-store/src/controllers/products/products.controller.ts
@@ -1,22 +1,31 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common'
 import { CreateProduct } from '@/types/products'
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  getProducts(@Query('limit') limit: number, @Query('offset') offset: number) {
+  getPage(@Query('limit') limit: number, @Query('offset') offset: number) {
     return {
       message: `products ${limit} ${offset}`,
     }
   }
 
   @Get('filter')
-  getProductFilter(): string {
+  getFilter(): string {
     return `hello filter`
   }
 
   @Get(':id')
-  getProduct(@Param('id') id: string) {
+  get(@Param('id') id: string) {
     return {
       message: `product ${id}`,
     }
@@ -26,6 +35,21 @@ export class ProductsController {
   create(@Body() payload: CreateProduct) {
     return {
       body: payload,
+    }
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: CreateProduct) {
+    return {
+      id,
+      payload,
+    }
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return {
+      id,
     }
   }
 }
