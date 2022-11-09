a24eae464ed0cb2a56833c2eec221caea211a42a
Carlosayol <Carlosaolar@gmail.com>
2022-11-09 18:31:33 -0500

    use validators in dtos to validate fields

--- nest/game-store/src/dtos/products.dtos.ts
+++ nest/game-store/src/dtos/products.dtos.ts
@@ -1,16 +1,39 @@
+import { PartialType } from '@nestjs/mapped-types'
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUrl,
+  IsUUID,
+} from 'class-validator'
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string
 }
 
-export class UpdateProductDto {
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @IsUUID()
+  @IsNotEmpty()
   readonly id: string
-  readonly name?: string
-  readonly description?: string
-  readonly price?: number
-  readonly stock?: number
-  readonly image?: string
 }
