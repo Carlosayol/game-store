6f685e194b30463941e59bac8ab66674927ff65a
Carlosayol <Carlosaolar@gmail.com>
2022-11-16 19:23:28 -0500

    implement usefactory

--- nest/game-store/src/app.module.ts
+++ nest/game-store/src/app.module.ts
@@ -3,16 +3,27 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { ProductsModule } from './products/products.module'
+import { HttpModule, HttpService } from '@nestjs/axios'
+import { firstValueFrom } from 'rxjs'
 
 const API_KEY = '1231232123'
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
       useValue: API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasksT = http.get('https://jsonplaceholder.typicode.com/todos')
+        const tasks = await firstValueFrom(tasksT)
+
+        return tasks.data
+      },
     },
   ],
 })
