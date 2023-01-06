16ed4bd32ad957403504af9b97cbf18bc2a16f18
Carlosayol <Carlosaolar@gmail.com>
2023-01-05 19:49:17 -0500

    complete brand crud

--- nest/game-store/src/products/services/brands/brands.service.ts
+++ nest/game-store/src/products/services/brands/brands.service.ts
@@ -1,8 +1,6 @@
 import { CreateBrandDto, UpdateBrandDto } from '../../dtos/brands.dto'
 import { Brand } from '../../entities/brand.entity'
-import { Inject, Injectable, NotFoundException } from '@nestjs/common'
-import { randomUUID } from 'crypto'
-import { Db } from 'mongodb'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 
@@ -23,38 +21,35 @@ export class BrandsService {
     return brand
   }
 
-  create(payload: CreateBrandDto) {
-    const newCategory = {
-      id: randomUUID(),
-      ...payload,
+  async create(payload: CreateBrandDto) {
+    const newBrand = new this.brandModel(payload)
+    return await newBrand.save()
+  }
+
+  async update(id: string, payload: UpdateBrandDto) {
+    const brand = await this.brandModel
+      .findByIdAndUpdate(id, { $set: payload }, { new: true })
+      .exec()
+
+    if (!brand) {
+      throw new NotFoundException(`Brand #${id} not found`)
     }
 
-    this.brands.push(newCategory)
-    return newCategory
+    return brand
   }
 
-  update(id: string, payload: UpdateBrandDto) {
-    const brand = this.find(id)
-    if (brand) {
-      const index = this.brands.findIndex((item) => item.id == id)
-      this.brands[index] = {
-        ...brand,
-        ...payload,
-      }
+  async delete(id: string) {
+    const brand = await this.brandModel.findByIdAndRemove(id)
 
-      return payload
+    if (!brand) {
+      throw new NotFoundException(`Brand #${id} not found`)
     }
 
-    throw new NotFoundException(`Brand #${id} not found`)
+    return brand
   }
 
-  delete(id: string) {
-    this.brands == this.brands.filter((item) => item.id != id)
-    return id
-  }
-
-  testDatabase() {
-    const tasksCollection = this.database.collection('tasks')
-    return tasksCollection.find().toArray()
-  }
+  // testDatabase() {
+  //   const tasksCollection = this.database.collection('tasks')
+  //   return tasksCollection.find().toArray()
+  // }
 }
