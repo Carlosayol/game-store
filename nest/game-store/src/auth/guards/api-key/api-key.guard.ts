823e37834938f520f9d0e857d7eea5bedcb900f3
Carlosayol <Carlosaolar@gmail.com>
2023-01-10 13:13:12 -0500

    use env variables for guards

--- nest/game-store/src/auth/guards/api-key/api-key.guard.ts
+++ nest/game-store/src/auth/guards/api-key/api-key.guard.ts
@@ -1,12 +1,14 @@
 import { IS_PUBLICK_KEY } from '@/auth/decorators/public.decorator'
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
+import config from '@/config'
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigType } from '@nestjs/config'
 import { Reflector } from '@nestjs/core'
 import { Request } from 'express'
 import { Observable } from 'rxjs'
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector, @Inject(config.KEY) private configService: ConfigType<typeof config>) {}
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.get(IS_PUBLICK_KEY, context.getHandler())
@@ -16,7 +18,7 @@ export class ApiKeyGuard implements CanActivate {
 
     const request = context.switchToHttp().getRequest<Request>()
     const authHeader = request.header('Auth')
-    const isAuth = authHeader === '1234'
+    const isAuth = authHeader === this.configService.apiKey
     if (!isAuth) {
       throw new UnauthorizedException('not allow')
     }
