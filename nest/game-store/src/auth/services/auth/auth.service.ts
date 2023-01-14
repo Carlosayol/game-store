9978ae59d8e756fefca07147517bb58981338732
Carlosayol <Carlosaolar@gmail.com>
2023-01-14 11:52:21 -0500

    connect passport with jwt

--- nest/game-store/src/auth/services/auth/auth.service.ts
+++ nest/game-store/src/auth/services/auth/auth.service.ts
@@ -1,10 +1,13 @@
+import { Token } from '@/auth/models/token.model'
+import { User } from '@/users/entities/user.entity'
 import { UsersService } from '@/users/services/users/users.service'
 import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.usersService.findByEmail(email)
@@ -19,5 +22,13 @@ export class AuthService {
     }
 
     return null
+  }
+
+  generateJWT(user: User) {
+    const payload: Token = { role: user.role, sub: user._id }
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    }
   }
 }
