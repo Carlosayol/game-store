a7a73684d63032368046ae73a15c68bc6b5075e9
Carlosayol <Carlosaolar@gmail.com>
2023-01-22 11:48:35 -0500

    reformat utils modules

--- nest/game-store/src/common/mongo-id/mongo-id.pipe.spec.ts
+++ nest/game-store/src/common/mongo-id/mongo-id.pipe.spec.ts
@@ -1,7 +1,7 @@
-import { MongoIdPipe } from './mongo-id.pipe';
+import { MongoIdPipe } from './mongo-id.pipe'
 
 describe('MongoIdPipe', () => {
   it('should be defined', () => {
-    expect(new MongoIdPipe()).toBeDefined();
-  });
-});
+    expect(new MongoIdPipe()).toBeDefined()
+  })
+})
